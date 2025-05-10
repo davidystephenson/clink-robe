17a1f67f7a12371259339e377dc9cf72adb15f55
David Y. Stephenson <david@davidystephenson.com>
2025-05-09 23:15:57 -0400

    Style active buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import { Heading } from "@chakra-ui/react"
+import { Heading, HStack } from "@chakra-ui/react"
 import ClinkRobe from "./ClinkRobe"
 import { ColorSwitchRobe, RobesProvider } from "robes"
 import ButtonClinkRobe from "./ButtonClinkRobe"
@@ -10,9 +10,11 @@ function App() {
       <BrowserRouter>
         <ColorSwitchRobe />
         <Heading>Hello World</Heading>
-        <ClinkRobe to="/">Home</ClinkRobe>
-        <ClinkRobe to="/test">Test</ClinkRobe>
-        <ButtonClinkRobe to="/test">Test</ButtonClinkRobe>
+        <HStack>
+          <ClinkRobe to="/">Home</ClinkRobe>
+          <ClinkRobe to="/test">Test</ClinkRobe>
+          <ButtonClinkRobe to="/test">Test</ButtonClinkRobe>
+        </HStack>
         <Routes>
           <Route path="/" element={<div>Home</div>} />
           <Route path="/test" element={<div>Test</div>} />
--- src/ButtonClinkRobe.tsx
+++ src/ButtonClinkRobe.tsx
@@ -6,9 +6,10 @@ import { Link, Link as RouterLink } from "react-router-dom"
 export default function ButtonClinkRobe (props: ButtonClinkRobeProps) {
   const { button, children, ...rest } = props
   const active = useActive(props)
+
   return (
     <Link as={RouterLink} {...rest}>
-      <ButtonRobe isDisabled={active} {...button}>
+      <ButtonRobe {...button} isActive={active}>
         {children}
       </ButtonRobe>
     </Link>
